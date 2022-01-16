c2b727523467dbd7fa5216b4e4d000880f9ab1ee
Bastian Müller <bastian@turbolent.com>
2022-01-15 22:12:03 -0800

    add Swift WASI example

---
+++ examples/swift-wasi/Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-wasi",
+    targets: [
+        .target(
+            name: "swift-wasi",
+            dependencies: []
+        )
+    ]
+)
---
+++ examples/swift-wasi/Sources/swift-wasi/main.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+print("Arguments (\(CommandLine.argc)): \(CommandLine.arguments)")
+
+print("Now: \(Date().timeIntervalSince1970)")
