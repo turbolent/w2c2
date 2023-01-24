06e58f75bcff073f14226b7ef301b06f8b3067d8
Bastian Müller <bastian@turbolent.com>
2023-01-23 21:22:29 -0800

    add Macintosh support to Python example

--- examples/python/main.c
+++ examples/python/main.c
@@ -1,4 +1,19 @@
+#if defined(__MWERKS__) && defined(macintosh)
+#include <MacMemory.h>
+#include <Files.h>
+#define __bool_true_false_are_defined
+typedef char bool;
+#endif
+
 #include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+
+#ifdef __MSL__
+#include <SIOUX.h>
+#include <console.h>
+#endif
+
 #include "../../w2c2/w2c2_base.h"
 #include "../../wasi/wasi.h"
 #include "python.h"
@@ -18,11 +33,25 @@ wasiMemory(
     return python_memory((pythonInstance*)instance);
 }
 
+#if defined(__MSL__) && defined(macintosh)
+char** environ = {NULL};
+#else
 extern char** environ;
+#endif
+
 
 /* Main */
 
 int main(int argc, char* argv[]) {
+
+#if defined(__MWERKS__) && defined(macintosh)
+    MaxApplZone();
+    MoreMasters();
+    MoreMasters();
+
+    argc = ccommand(&argv);
+#endif
+
     /* Initialize WASI */
     if (!wasiInit(argc, argv, environ)) {
         fprintf(stderr, "failed to init WASI\n");
@@ -33,6 +62,10 @@ int main(int argc, char* argv[]) {
         fprintf(stderr, "failed to add preopen\n");
         return 1;
     }
+
+#ifdef __MSL__
+    SIOUXSetTitle("\pPython");
+#endif
 
     {
         pythonInstance instance;
