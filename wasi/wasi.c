53b4f3ccdb6608bbba6a0b43ac40dd22f810497b
Bastian Müller <bastian@turbolent.com>
2023-02-04 13:49:45 -0800

    NeXTSTEP's unistd does not define file numbers

--- wasi/wasi.c
+++ wasi/wasi.c
@@ -117,6 +117,13 @@ struct timespec {
 #define STDIN_FILENO  0
 #define STDOUT_FILENO 1
 #define STDERR_FILENO 2
+#endif /* __MSL__ */
+
+#ifdef __NeXT__
+/* Missing in unistd.h */
+#define STDIN_FILENO  0
+#define STDOUT_FILENO 1
+#define STDERR_FILENO 2
 #endif
 
 #ifndef PATH_MAX
