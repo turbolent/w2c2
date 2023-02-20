eca26c9fb98dafee350ee0b1de087469be379f14
Bastian Müller <bastian@turbolent.com>
2023-02-19 17:50:56 -0800

    manually convert Windows error to POSIX errno

--- wasi/win32.c
+++ wasi/win32.c
@@ -1,19 +1,185 @@
 #ifdef _WIN32
 #include "win32.h"
 
-#if defined(_MSC_VER) && (_MSC_VER < 1900)
-void __cdecl _dosmaperr(unsigned long);
+/*
+ * Taken from PostgreSQL.
+ * Portions Copyright (c) 1996-2022, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1994, The Regents of the University of California
+ */
 
-static void SetErrno() {
-    _dosmaperr(GetLastError());
-}
-#else
-void __cdecl __acrt_errno_map_os_error(unsigned long const oserrno);
+static const struct {
+    DWORD winerr;
+    int doserr;
+} doserrors[] = {
+    {
+        ERROR_INVALID_FUNCTION, EINVAL
+    },
+    {
+        ERROR_FILE_NOT_FOUND, ENOENT
+    },
+    {
+        ERROR_PATH_NOT_FOUND, ENOENT
+    },
+    {
+        ERROR_TOO_MANY_OPEN_FILES, EMFILE
+    },
+    {
+        ERROR_ACCESS_DENIED, EACCES
+    },
+    {
+        ERROR_INVALID_HANDLE, EBADF
+    },
+    {
+        ERROR_ARENA_TRASHED, ENOMEM
+    },
+    {
+        ERROR_NOT_ENOUGH_MEMORY, ENOMEM
+    },
+    {
+        ERROR_INVALID_BLOCK, ENOMEM
+    },
+    {
+        ERROR_BAD_ENVIRONMENT, E2BIG
+    },
+    {
+        ERROR_BAD_FORMAT, ENOEXEC
+    },
+    {
+        ERROR_INVALID_ACCESS, EINVAL
+    },
+    {
+        ERROR_INVALID_DATA, EINVAL
+    },
+    {
+        ERROR_INVALID_DRIVE, ENOENT
+    },
+    {
+        ERROR_CURRENT_DIRECTORY, EACCES
+    },
+    {
+        ERROR_NOT_SAME_DEVICE, EXDEV
+    },
+    {
+        ERROR_NO_MORE_FILES, ENOENT
+    },
+    {
+        ERROR_LOCK_VIOLATION, EACCES
+    },
+    {
+        ERROR_SHARING_VIOLATION, EACCES
+    },
+    {
+        ERROR_BAD_NETPATH, ENOENT
+    },
+    {
+        ERROR_NETWORK_ACCESS_DENIED, EACCES
+    },
+    {
+        ERROR_BAD_NET_NAME, ENOENT
+    },
+    {
+        ERROR_FILE_EXISTS, EEXIST
+    },
+    {
+        ERROR_CANNOT_MAKE, EACCES
+    },
+    {
+        ERROR_FAIL_I24, EACCES
+    },
+    {
+        ERROR_INVALID_PARAMETER, EINVAL
+    },
+    {
+        ERROR_NO_PROC_SLOTS, EAGAIN
+    },
+    {
+        ERROR_DRIVE_LOCKED, EACCES
+    },
+    {
+        ERROR_BROKEN_PIPE, EPIPE
+    },
+    {
+        ERROR_DISK_FULL, ENOSPC
+    },
+    {
+        ERROR_INVALID_TARGET_HANDLE, EBADF
+    },
+    {
+        ERROR_INVALID_HANDLE, EINVAL
+    },
+    {
+        ERROR_WAIT_NO_CHILDREN, ECHILD
+    },
+    {
+        ERROR_CHILD_NOT_COMPLETE, ECHILD
+    },
+    {
+        ERROR_DIRECT_ACCESS_HANDLE, EBADF
+    },
+    {
+        ERROR_NEGATIVE_SEEK, EINVAL
+    },
+    {
+        ERROR_SEEK_ON_DEVICE, EACCES
+    },
+    {
+        ERROR_DIR_NOT_EMPTY, ENOTEMPTY
+    },
+    {
+        ERROR_NOT_LOCKED, EACCES
+    },
+    {
+        ERROR_BAD_PATHNAME, ENOENT
+    },
+    {
+        ERROR_MAX_THRDS_REACHED, EAGAIN
+    },
+    {
+        ERROR_LOCK_FAILED, EACCES
+    },
+    {
+        ERROR_ALREADY_EXISTS, EEXIST
+    },
+    {
+        ERROR_FILENAME_EXCED_RANGE, ENAMETOOLONG
+    },
+    {
+        ERROR_NESTING_NOT_ALLOWED, EAGAIN
+    },
+    {
+        ERROR_NOT_ENOUGH_QUOTA, ENOMEM
+    },
+    {
+        ERROR_DELETE_PENDING, ENOENT
+    },
+    {
+        ERROR_INVALID_NAME, ENOENT
+    },
+    {
+        ERROR_CANT_RESOLVE_FILENAME, ENOENT
+    }
+};
 
-static void SetErrno() {
-    __acrt_errno_map_os_error(GetLastError());
+static
+void
+setErrno(
+   unsigned long e
+) {
+    int i;
+    if (e == 0) {
+        errno = 0;
+        return;
+    }
+
+    for (i = 0; i < (sizeof(doserrors) / sizeof(doserrors[0])); i++) {
+        if (doserrors[i].winerr == e) {
+            errno = doserrors[i].doserr;
+            return;
+        }
+    }
+
+    errno = EINVAL;
 }
-#endif
 
 /*
  * Taken from PostgreSQL.
@@ -22,45 +188,44 @@ static void SetErrno() {
  */
 
 DIR *
-opendir(const char *dirname)
-{
-    DWORD		attr;
-    DIR		   *d;
+opendir(
+    const char* dirname
+) {
+    DWORD attr;
+    DIR *d;
 
     /* Make sure it is a directory */
     attr = GetFileAttributes(dirname);
-    if (attr == INVALID_FILE_ATTRIBUTES)
-    {
+    if (attr == INVALID_FILE_ATTRIBUTES) {
         errno = ENOENT;
         return NULL;
     }
-    if ((attr & FILE_ATTRIBUTE_DIRECTORY) != FILE_ATTRIBUTE_DIRECTORY)
-    {
+    if ((attr & FILE_ATTRIBUTE_DIRECTORY) != FILE_ATTRIBUTE_DIRECTORY) {
         errno = ENOTDIR;
         return NULL;
     }
 
     d = malloc(sizeof(DIR));
-    if (!d)
-    {
+    if (!d) {
         errno = ENOMEM;
         return NULL;
     }
     d->dirname = malloc(strlen(dirname) + 4);
-    if (!d->dirname)
-    {
+    if (!d->dirname) {
         errno = ENOMEM;
         free(d);
         return NULL;
     }
     strcpy(d->dirname, dirname);
     if (d->dirname[strlen(d->dirname) - 1] != '/' &&
-        d->dirname[strlen(d->dirname) - 1] != '\\')
-        strcat(d->dirname, "\\");	/* Append backslash if not already there */
-    strcat(d->dirname, "*");	/* Search for entries named anything */
+        d->dirname[strlen(d->dirname) - 1] != '\\') {
+
+        strcat(d->dirname, "\\"); /* Append backslash if not already there */
+    }
+    strcat(d->dirname, "*"); /* Search for entries named anything */
     d->handle = INVALID_HANDLE_VALUE;
-    d->ret.d_ino = 0;			/* no inodes on win32 */
-    d->ret.d_reclen = 0;		/* not used on win32 */
+    d->ret.d_ino = 0; /* no inodes on win32 */
+    d->ret.d_reclen = 0; /* not used on win32 */
     d->ret.d_type = DT_UNKNOWN;
 
     return d;
@@ -72,37 +237,38 @@ opendir(const char *dirname)
  * Portions Copyright (c) 1994, The Regents of the University of California
  */
 
-struct dirent *
-readdir(DIR *d)
-{
+struct dirent*
+readdir(
+    DIR* d
+) {
     WIN32_FIND_DATA fd;
 
-    if (d->handle == INVALID_HANDLE_VALUE)
-    {
+    if (d->handle == INVALID_HANDLE_VALUE) {
         d->handle = FindFirstFile(d->dirname, &fd);
-        if (d->handle == INVALID_HANDLE_VALUE)
-        {
+
+        if (d->handle == INVALID_HANDLE_VALUE) {
+            unsigned long e = GetLastError();
             /* If there are no files, force errno=0 (unlike mingw) */
-            if (GetLastError() == ERROR_FILE_NOT_FOUND)
+            if (e == ERROR_FILE_NOT_FOUND) {
                 errno = 0;
-            else
-                SetErrno();
+            } else {
+                setErrno(e);
+            }
             return NULL;
         }
-    }
-    else
-    {
-        if (!FindNextFile(d->handle, &fd))
-        {
+    } else {
+        if (!FindNextFile(d->handle, &fd)) {
+            unsigned long e = GetLastError();
             /* If there are no more files, force errno=0 (like mingw) */
-            if (GetLastError() == ERROR_NO_MORE_FILES)
+            if (e == ERROR_NO_MORE_FILES)
                 errno = 0;
-            else
-                SetErrno();
+            else {
+                setErrno(e);
+            }
             return NULL;
         }
     }
-    strcpy(d->ret.d_name, fd.cFileName);	/* Both strings are MAX_PATH long */
+    strcpy(d->ret.d_name, fd.cFileName); /* Both strings are MAX_PATH long */
     d->ret.d_namlen = strlen(d->ret.d_name);
 
     /*
@@ -111,13 +277,14 @@ readdir(DIR *d)
      * directories.
      */
     if ((fd.dwFileAttributes & FILE_ATTRIBUTE_REPARSE_POINT) != 0 &&
-        (fd.dwReserved0 == IO_REPARSE_TAG_MOUNT_POINT))
-        d->ret.d_type = DT_LNK;
-    else if ((fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) != 0)
-        d->ret.d_type = DT_DIR;
-    else
-        d->ret.d_type = DT_REG;
+        (fd.dwReserved0 == IO_REPARSE_TAG_MOUNT_POINT)) {
 
+        d->ret.d_type = DT_LNK;
+    } else if ((fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) != 0) {
+        d->ret.d_type = DT_DIR;
+    } else {
+        d->ret.d_type = DT_REG;
+    }
     return &d->ret;
 }
 
@@ -128,12 +295,14 @@ readdir(DIR *d)
  */
 
 int
-closedir(DIR *d)
-{
-    int			ret = 0;
+closedir(
+    DIR* d
+) {
+    int ret = 0;
 
-    if (d->handle != INVALID_HANDLE_VALUE)
+    if (d->handle != INVALID_HANDLE_VALUE) {
         ret = !FindClose(d->handle);
+    }
     free(d->dirname);
     free(d);
 
