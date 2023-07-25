ceb34dc85872878e7cbc94475223c0664745c852
Bastian Müller <bastian@turbolent.com>
2023-07-24 20:39:57 -0700

    add support for expected failures

---
+++ tests/run.py
@@ -0,0 +1,46 @@
+import argparse
+import subprocess
+import os
+import difflib
+import sys
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('test')
+    parser.add_argument('-a', '--arch')
+    parser.add_argument('-e', '--execute')
+    args = parser.parse_args()
+
+    command = []
+
+    if args.execute:
+        command.append(args.execute)
+    command.append(os.path.abspath(args.test))
+
+    output = subprocess.run(command, check=True, stderr=subprocess.PIPE).stderr.decode('utf-8').strip()
+
+    print(output)
+
+    if args.arch:
+        path = args.test + '.' + args.arch + '.out'
+        if os.path.isfile(path):
+            expected = ''
+            with open(path, 'r') as f:
+                expected = f.read()
+            diff = list(difflib.unified_diff(
+                expected.splitlines(keepends=True),
+                output.splitlines(keepends=True),
+                fromfile='expected',
+                tofile='actual'
+            ))
+            if diff:
+                print()
+                sys.stdout.writelines(diff)
+                exit(1)
+            return
+
+    if 'FAIL: ' in output:
+        exit(1)
+
+if __name__ == "__main__":
+    main()
