turbolent/w2c2

commit 65f19972bef7b4670935d210f554aeb93469ad66

make memories pointers, so they can be shared across instances. implement memory.init

w2c2/w2c2_base.h
@@ -552,9 +552,9 @@ DEFINE_REINTERPRET(i64_reinterpret_f64, F64, U64)
 
 typedef struct wasmMemory {
     U8* data;
+    U32 size;
     U32 pages;
     U32 maxPages;
-    U32 size;
     bool shared;
 #ifdef WASM_MUTEX_TYPE
     WASM_MUTEX_TYPE mutex;
@@ -565,40 +565,46 @@ typedef struct wasmMemory {
 
 static
 W2C2_INLINE
-void
+wasmMemory*
 wasmMemoryAllocate(
-    wasmMemory* memory,
     const U32 initialPages,
     const U32 maxPages
 ) {
     const U32 size = initialPages * WASM_PAGE_SIZE;
+    wasmMemory* memory = calloc(1, sizeof(wasmMemory));
+    if (!memory) {
+        abort();
+    }
     memory->data = calloc(size, 1);
     memory->size = size;
     memory->pages = initialPages;
     memory->maxPages = maxPages;
     memory->shared = false;
+    return memory;
 }
 
 static
 W2C2_INLINE
-void
+wasmMemory*
 wasmMemoryAllocateShared(
-    wasmMemory* memory,
     const U32 initialPages,
     const U32 maxPages
 ) {
-    wasmMemoryAllocate(memory, initialPages, maxPages);
+    wasmMemory* memory = wasmMemoryAllocate(initialPages, maxPages);
     memory->shared = true;
 #ifdef WASM_MUTEX_TYPE
-    WASM_MUTEX_INIT(&memory->mutex);
+    if (!WASM_MUTEX_INIT(&memory->mutex)) {
+        abort();
+    }
 #else
     abort();
 #endif
+    return memory;
 }
 
 #ifdef WASM_MUTEX_TYPE
-#define WASM_MEMORY_ALLOCATE_SHARED(memory, initialPages, maxPages) \
-    wasmMemoryAllocateShared(memory, initialPages, maxPages)
+#define WASM_MEMORY_ALLOCATE_SHARED(initialPages, maxPages) \
+    wasmMemoryAllocateShared(initialPages, maxPages)
 #else
 #define WASM_MEMORY_ALLOCATE_SHARED(memory, initialPages, maxPages) \
     _Pragma ("GCC error \"Shared memory not supported. Please define a mutex implementation to use (WASM_MUTEX_*)\"")

tests/main.c
@@ -14,8 +14,8 @@ void spectest__print_i32(U32 l0) {
 }
 
 static wasmTable spectest_table;
-static wasmMemory spectest_memory;
-static wasmMemory spectest_shared_memory;
+static wasmMemory* spectest_memory;
+static wasmMemory* spectest_shared_memory;
 
 void*
 resolveTestImports(
@@ -117,8 +117,8 @@ void printOK(const char* description) {
 }
 
 static void initTest() {
-    wasmMemoryAllocate(&spectest_memory, 1, 2);
-    wasmMemoryAllocate(&spectest_shared_memory, 1, 2);
+    spectest_memory = wasmMemoryAllocate(1, 2);
+    spectest_shared_memory = wasmMemoryAllocateShared(1, 2);
     wasmTableAllocate(&spectest_table, 10, 20);
 }
 

w2c2/instruction.c
@@ -190,3 +190,23 @@ wasmMemoryCopyInstructionRead(
 
     return true;
 }
+
+/* WasmMemoryInitInstruction */
+
+bool
+WARN_UNUSED_RESULT
+wasmMemoryInitInstructionRead(
+    Buffer* buffer,
+    WasmMemoryInitInstruction* result
+) {
+    U32 dataSegmentIndex = 0;
+    U32 memoryIndex = 0;
+
+    MUST (leb128ReadU32(buffer, &dataSegmentIndex) > 0)
+    MUST (leb128ReadU32(buffer, &memoryIndex) > 0)
+
+    result->dataSegmentIndex = dataSegmentIndex;
+    result->memoryIndex = memoryIndex;
+
+    return true;
+}

w2c2/instruction.h
@@ -206,4 +206,18 @@ wasmMemoryCopyInstructionRead(
     WasmMemoryCopyInstruction* result
 );
 
+/* WasmMemoryInitInstruction */
+
+typedef struct WasmMemoryInitInstruction {
+    U32 dataSegmentIndex;
+    U32 memoryIndex;
+} WasmMemoryInitInstruction;
+
+bool
+WARN_UNUSED_RESULT
+wasmMemoryInitInstructionRead(
+    Buffer* buffer,
+    WasmMemoryInitInstruction* result
+);
+
 #endif /* W2C2_INSTRUCTION_H */

commit 4a6288565fc216556b05863d0e996b565ea9bfea

free memory itself
fix regression introduced in 65f1997

w2c2/w2c2_base.h
@@ -769,6 +769,8 @@ wasmMemoryFree(
     }
     memory->futex = NULL;
     memory->futexFree = NULL;
+
+    free(memory);
 }
 
 static